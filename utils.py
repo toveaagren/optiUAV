c8ff80c8b5e35717039fd60b756bcab6e88f5117
tagren <tove.s.aagren@nasa.gov>
2024-06-18 10:48:35 -0700

    corrected coefficient expansion

--- utils.py
+++ utils.py
@@ -27,18 +27,18 @@ def unique_with_tolerance(arr, tol=1e-8):
     return sorted_arr[unique_indices]
 
 
-def unpack_coefficients(
-    q: np.ndarray, n_state: int, n_input: int, n_colloc: int
-) -> tuple:
-    a_ = np.zeros(
-        (n_state, n_colloc)
-    )  # These are the state coefficients a_k, i.e., the k_th column is a the k_th column is a_k
-    b_ = np.zeros((n_input, n_colloc))
-    end_indx = n_state * n_colloc
-    for k in range(n_colloc):
-        a_[:, k] = q[n_state * k : n_state * (k + 1)]
-    for k in range(n_colloc):
-        b_[:, k] = q[
-            (end_indx + n_input * k) : (end_indx + n_input * (k + 1))
-        ]  # %Similarly, these are the input coefficients b_k
-    return a_, b_
+# def unpack_coefficients(
+#     q: np.ndarray, n_state: int, n_input: int, n_colloc: int
+# ) -> tuple:
+#     a_ = np.zeros(
+#         (n_state, n_colloc)
+#     )  # These are the state coefficients a_k, i.e., the k_th column is a the k_th column is a_k
+#     b_ = np.zeros((n_input, n_colloc))
+#     end_indx = n_state * n_colloc
+#     for k in range(n_colloc):
+#         a_[:, k] = q[n_state * k : n_state * (k + 1)]
+#     for k in range(n_colloc):
+#         b_[:, k] = q[
+#             (end_indx + n_input * k) : (end_indx + n_input * (k + 1))
+#         ]  # %Similarly, these are the input coefficients b_k
+#     return a_, b_
